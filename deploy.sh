fd1de27bb2f8618e810b4fd00be488717644cc2c
Andrew Langmeier <raymi306@gmail.com>
2024-01-28 18:22:11 -0500

    Fix deploy script zipping; Recenter h1 on mobile

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 zola build &&
-    gzip -vrk9 public/*.{html,css,js,txt,xml} &&
+    gzip -vrk9 --rsyncable public &&
     rsync -rvz --progress -e 'ssh -p 57018' public/* andrew@let-them.cyou:/srv/www/lt/andrew
