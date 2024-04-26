24e45f68f79c58cfb379c54152fbf83ede521407
Andrew Langmeier <raymi306@gmail.com>
2024-04-25 23:59:37 -0400

    Stub out deploy.sh a little

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+# Untested as I am not deploying manually anymore
+SSH_PORT=xxxx
+USER=xxxx
+HOST=xxxx
+TARGET_DIR=xxxx
+
 echo 'Did you run `zola check`?'
 zola build &&
     gzip -vrk9 --rsyncable public &&
-    rsync -rvz --progress -e 'ssh -p 57018' public/* andrew@let-them.cyou:/srv/www/lt/andrew
+    rsync -rvz --progress -e 'ssh -p $SSH_PORT' public/* $USER@$HOST:$TARGET_DIR
